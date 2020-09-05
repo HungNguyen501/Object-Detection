270a363cb48afa72ebf871f733969e47522fb8d9
Nguyen Dinh Hung <chelseazodoi@gmail.com>
2020-09-05 17:17:37 +0700

    upload project

---
+++ main.py
@@ -0,0 +1,36 @@
+import cv2
+
+thres = 0.5 #threshold to detect object
+
+cap = cv2.VideoCapture(0, cv2.CAP_MSMF)
+cap.set(3,640)
+cap.set(4,480)
+
+classNames = []
+classFile = 'object_labels.names'
+with open(classFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+#print(classNames)
+
+configPath = 'ssd_mobilenet_v3_large.pbtxt'
+weightspath = 'pre_trained_model.pb'
+
+net = cv2.dnn_DetectionModel(weightspath, configPath)
+net.setInputSize(320, 320)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+while True:
+    success, img = cap.read()
+    classIds, confs, bbox = net.detect(img, confThreshold = thres)
+    #print(classIds, bbox)
+
+    if len(classIds) != 0:
+        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox): 
+            cv2.rectangle(img, box, color=(255, 0, 0), thickness = 2)
+            label = "{}: {:.2f}%".format( classNames[classId-1], confidence * 100)
+            cv2.putText(img, label, (box[0] + 0, box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
+    cv2.imshow("Output", img)
+    cv2.waitKey(1)
